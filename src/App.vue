7e93cf8d9a79991f75c53c081e522d9dda26148b
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-04-14 18:51:48 +0200

    starting Front-End configuration

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
+    <div>
+        <SideMenu/>
+    </div>
 
 </template>
 
 <script>
     import axios from 'axios'
     import { store } from './store'
+    import SideMenu from "./components/SideMenu.vue"
     
     export default {
+        components: {
+            SideMenu
+        },
         data() {
             return{
                 store,
@@ -21,5 +28,11 @@
 <style lang="scss">
 
 @use "./styles/general.scss";
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
 </style>
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="mg-sidebar">
+        
+        <div class="mg-realMenu border-black">
+            <div class="leftspace"></div>
+            <div class="content">
+                <div class="top-content">
+                    <h5>Holocaust Centre North</h5>
+                </div>
+
+            </div>
+            <div class="rightspace"></div>
+            
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SideMenu",
+    setup () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .mg-sidebar{
+        padding: 15px 0px 15px 15px;
+        height: 100vh;
+        width: calc(100%/4);
+        
+        .mg-realMenu{
+            width: 100%;
+            height: 100%;
+            
+            border: 1px solid black;
+            
+            position: relative;
+            .content{
+                background-color: coral;
+                height: 100%;
+                padding: 0 15px;
+
+                .top-content{
+                    padding: 15px 0;
+                }
+            }
+            .leftspace,
+            .rightspace{
+                background-color: white;
+                height: 101%;
+                width: 15px;
+                position: absolute;
+            }
+            .leftspace{
+                top: -1px;
+            }
+            .rightspace{
+                top: -1px;
+                right: 0;
+            }
+        }
+
+        
+    }
+    
+</style>
